ae7155b2c3c6b19941332bc38b3c5519758c2d88
asyhaha <aisyahsapari16@gmail.com>
2025-07-16 16:43:15 +0800

    Delete FoodTruckTracker directory

--- FoodTruckTracker/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-subprojects {
-    configurations.all {
-        resolutionStrategy.eachDependency {
-            if (requested.group == "org.jetbrains.kotlin") {
-                useVersion("1.8.10")
-            }
-        }
-    }
-}
